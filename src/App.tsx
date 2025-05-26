45a133449d5bb4e88f3f2f4329de2ed9d74576c1
kleinalex-01 <kleinalex215@gmail.com>
2025-05-26 18:42:38 +0200

    Item listing func
    Add items and delete all items buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getAnalytics } from "firebase/analytics";
 import { firebaseApp } from './firebaseConfig'
 import { InputBar } from './assets/InputBar/inputBar'
 function App() {
-  const analytics = getAnalytics(firebaseApp);
-  const [item, setItem] = useState([]);
+  useEffect(() => {
+    getAnalytics(firebaseApp);
+  },[])
+
+  const [item, setItem] = useState<string[]>([]);
   return (
     <>
       <InputBar item={item} setItem={setItem}/>
--- src/assets/InputBar/inputBar.tsx
+++ src/assets/InputBar/inputBar.tsx
@@ -1,47 +1,64 @@
 import { useState } from "react";
 
 interface Props {
-    item: string;
-    setItem: (value: string) => void
+    item: string[];
+    setItem: React.Dispatch<React.SetStateAction<string[]>>
 }
 export const InputBar = ({item, setItem}: Props) => {
-
-    const [inputVal, setInputVal] = useState("")
+    const [inputVal, setInputVal] = useState<string>("")
     const addItem = (e: React.FormEvent) => {
-        e.preventDefault();
-        
+        e.preventDefault()
+        if (inputVal.trim() === "") return
+
+        setItem([...item, inputVal])
+
+        setInputVal("")
     }
+
+    const deleteAll = () => {
+        setItem([])
+    }
+
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <div className="container my-3">
+         <div className="container my-3">
+            <form onSubmit={addItem}>
                 <div className="row mb-3">
                     <div className="col-12">
-                            <input type="text"
-                                    className="form-control"
-                                    placeholder="Eg. Mop floor..."
-                            />
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Eg. Mop floor..."
+                          value={inputVal}
+                          onChange={(e) => setInputVal(e.target.value)}
+                        />
                     </div>
+                </div>
+
                 <div className="row g-2">
                     <div className="col-4">
-                        <button className="btn btn-success w-100">Save Input</button>
-                    </div>
-
-                    <div className="col-4">
-                        <button className="btn btn-success w-100">Save Tab</button>
+                        <button type="submit" className="btn btn-success w-100">Save Input</button>
                     </div>
                     <div className="col-4">
-                        <button className="btn btn-light border-success w-100">Delete All</button>
+                        <button type="button" className="btn btn-success w-100">Save Tab</button>
+                    </div>
+                    <div className="col-4">
+                        <button type="button" onClick={deleteAll} className="btn btn-light border-success w-100">Delete All</button>
                     </div>
                 </div>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
 
         <div className="container">
             <div className="row">
                 <div className="col-6">
-                    <p className="h1">{item}</p>
+                    {item.map(item => {
+                        return(
+                            <>
+                                <li key={item} className="h1">{item}</li>
+                            </>
+                        )
+                    })}
                 </div>
             </div>
         </div>
