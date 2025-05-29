34f20ab3aba192d0051f29f5ec07cc8d1fbdd98c
kleinalex-01 <kleinalex215@gmail.com>
2025-05-29 12:13:30 +0200

    Update firebaseconfig.tsx

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -9,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
+    databaseURL: "https://leads-tracker-3225d-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
